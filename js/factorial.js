a13aca75d62d7b2a415bc53c9c69f8515e8ee7d4
Crusader-RG <justin.leeper92@gmail.com>
2024-05-26 06:19:10 -0600

    changed both files to arrow notation

--- js/factorial.js
+++ js/factorial.js
@@ -1,7 +1,9 @@
-exports.factorial = function(num) {
+const factorial = (num) => {
     let answer = 1
     for (i = num; i > 0; i--) {
         answer = answer * i
     }
     return answer
 };
+
+module.exports = factorial;
